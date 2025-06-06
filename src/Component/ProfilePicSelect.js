ce2536c6c4fc4766488a88743567a6e079686585
Jonathan-Git-Hub-dev <Jonathan.d.pincus@gmail.com>
2025-06-06 13:33:21 +0800

    Php saves profilePicture

--- src/Component/ProfilePicSelect.js
+++ src/Component/ProfilePicSelect.js
@@ -1,29 +1,99 @@
-import {useRef} from 'react';
+import {useRef, useState} from 'react';
 import "../CSS/ProfilePicSelect.css"
-import logo from '../im2.jpg';//import logo from '../im.avif';
+//import logo from '../im2.jpg';//import logo from '../im.avif';
 import Modal from './Modal.js';
+import $ from 'jquery';
 
 export default function ProfilePicSelect(props)
 {
     const modalHoldImageRef = useRef(null);
+    const [image, setImage] = useState(null);
+
+    function sendToServer()
+    {
+        let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+        let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+        let ScreenMin = Math.min(vw, vh);
+        
+        //finding position of the circle in the image for later cropping
+        console.log("Cirlce size: " + ScreenMin * 0.4);
+        console.log("Distance from top: " + (modalHoldImageRef.current.scrollTop));// - (ScreenMin * 0.1)));
+        console.log("Distance from left: " + (modalHoldImageRef.current.scrollLeft));// - (ScreenMin * 0.1)));
+
+    }
+
+    function getImage()
+    {
+        document.getElementById("file").click();
+    }
+
+    function imageSelected()
+    {
+        console.log("image");
+
+        //check if changed to relevant data type
+      var image = document.getElementById("file").files[0];
+
+      var reader = new FileReader();
+
+      reader.onload = function(e)
+      {
+        setImage(e.target.result);
+      }
+
+      reader.readAsDataURL(image);
+    }
 
     return (
         <Modal buttonText="launch modal">
             <div className="profilePicSelectContent">
                     <div ref={modalHoldImageRef} className="profilePicSelectHoldImage">
                         <div className="profilePicSelectCircle"></div>
-                        <img className="profilePicSelectImageStyle" src={logo}></img>
+                        <img className="profilePicSelectImageStyle" src={image}></img>
                     </div>
+                    <button onClick={sendToServer}>Print size</button>
+                    <button onClick={getImage}>choose image</button>
                     <button onClick={()=>{
+                        console.log("files");
+                        var file_data = $('#file').prop('files')[0];   
+                        var form_data = new FormData();                  
+                        form_data.append('file', file_data);
+                        
+
                         let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
                         let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
                         let ScreenMin = Math.min(vw, vh);
                         
                         //finding position of the circle in the image for later cropping
-                        console.log("Cirlce size: " + ScreenMin * 0.4);
-                        console.log("Distance from top: " + (modalHoldImageRef.current.scrollTop));// - (ScreenMin * 0.1)));
-                        console.log("Distance from left: " + (modalHoldImageRef.current.scrollLeft));// - (ScreenMin * 0.1)));
-                    }}>Print size</button>
+                        //console.log("Cirlce size: " + ScreenMin * 0.4);
+                        //console.log("Distance from top: " + (modalHoldImageRef.current.scrollTop));// - (ScreenMin * 0.1)));
+                        //console.log("Distance from left: " + (modalHoldImageRef.current.scrollLeft));// - (ScreenMin * 0.1)));
+
+                        form_data.append('c', ScreenMin * 0.4);
+                        form_data.append('x', (modalHoldImageRef.current.scrollLeft));
+                        form_data.append('y', (modalHoldImageRef.current.scrollTop));
+                        
+
+                        $.ajax({
+                            url: "http://localhost/profile/src/PHP/cropImage.php",
+                            dataType: 'text',  // <-- what to expect back from the PHP script, if anything
+                            cache: false,
+                            contentType: false,
+                            processData: false,
+                            data: form_data,                         
+                            type: 'post'
+                        })
+                        .done(function(data)
+                        {
+                            console.log(data);
+                        })
+                        .fail(function(data){
+                            console.log("Ajax error:\n" + data);
+                        });
+
+                    }}>php</button>
+                    <input type="file" id="file" onChange={imageSelected} accept="image/jpg, image/jpeg" hidden/>
+                        
             </div>
         </Modal>
     );
---
+++ src/PHP/cropImage.php
@@ -0,0 +1,80 @@
+<?php 
+header("Access-Control-Allow-Origin: *");//can be changed to be more specific
+
+//echo "crop here". $_POST["u"];
+
+//if(isset($_FILES['file']))
+//{ 
+    $file = $_FILES['file'];
+
+    $fileName = $_FILES['file']['name'];
+
+    $fileTempName = $_FILES['file']['tmp_name'];
+    $fileSize = $_FILES['file']['size'];
+    $fileError = $_FILES['file']['error'];
+    $fileType = $_FILES['file']['type'];
+    
+    /*$fileExt = explode('.', $fileName);
+    $fileActualExt = strtolower(end($fileExt));
+
+    $allowed = array('jpg', 'jpeg');
+
+    if(in_array($fileActualExt, $allowed))
+    {*/
+      if($fileError === 0)
+      {
+
+          //temporary save 
+          //$newName = "../a1/src/ProfilePic/UsersProfilePic." . $fileActualExt;
+          $newName="./tmp.jpg";
+          if(!move_uploaded_file($fileTempName, $newName))
+          {
+            //finish("-1");
+          }
+
+          //reload image for reformating
+          $im = imagecreatefromjpeg($newName);
+          if(!$im)
+          {
+            //finish("-1");
+          }  
+
+          //$s = min(imagesx($im), imagesy($im)); 
+          $x = $_POST['x'];
+          $y = $_POST['y'];
+          $c = $_POST['c'];
+          
+          $im2 = imagecrop($im, ['x' => $x, 'y' => $y, 'width' => $c, 'height' => $c]); 
+          if(!$im2)
+          {
+            ///finish("-1");
+          }
+
+          $im3 = imagescale ( $im2 , 200 , 200 );
+          if(!$im3)
+          {
+            //finish("-1");
+          }
+          
+          $newFN = "./temp.jpeg";
+          if(!imagejpeg($im3, $newFN, 100))
+          {
+            //finish("-1");
+          }
+      }
+      /*else
+      {
+        finish("-1");//ranodm number
+      }*/
+    /*}
+    else
+    {
+      finish("-6");//random number
+    }*/
+
+/*}
+else
+{
+  finish("-8");
+}*/
+
