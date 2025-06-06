25e32bfcd4c20e602ebc3f1ba8bd0defb1273ae4
Jonathan-Git-Hub-dev <Jonathan.d.pincus@gmail.com>
2025-06-06 10:34:58 +0800

    calculate profile position

--- src/Component/Test.js
+++ src/Component/Test.js
@@ -1,10 +1,11 @@
 import {useRef} from 'react';
 import "../CSS/modal.css"
 import logo from '../im2.jpg';
-
+//import logo from '../im.avif';
 export default function Test(props)
 {
     const modalRef = useRef(null);
+    const modalHoldImageRef = useRef(null);
 
     function prin()
     {
@@ -31,18 +32,25 @@ export default function Test(props)
                 //console.log(e.target);
             }}>
                 <div className="modalContent">
-                    <div className="modalHoldImage">
+                    <div ref={modalHoldImageRef} className="modalHoldImage">
                         <div className="circle"></div>
-                        <img src={logo}></img>
+                        <img className="imageStyle" src={logo}></img>
                     </div>
                     <button onClick={()=>{
                         let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
                         let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
-                        console.log("Height: " + vh + ",Width: " +vw);
-                    }}>Print size</button>
+                        //console.log("Height: " + vh + ",Width: " +vw);
                     
-
-
+                        let ScreenMin = Math.min(vw, vh);                      
+                        console.log("Cirlce size: " + ScreenMin * 0.4);
+                        console.log("Distance from top: " + (modalHoldImageRef.current.scrollTop));// - (ScreenMin * 0.1)));
+                        console.log("Distance from left: " + (modalHoldImageRef.current.scrollLeft));// - (ScreenMin * 0.1)));
+                        //modalRef.current
+                        //console.log("Client height" + modalHoldImageRef.current.clientHeight);
+                        //console.log("Scroll height" + modalHoldImageRef.current.scrollHeight);
+                        //console.log("Scroll Top" + modalHoldImageRef.current.scrollTop);
+                        //console.log("Top left corner X,Y: " + +"," +);
+                    }}>Print size</button>
                 </div>
             </div>
         </>
